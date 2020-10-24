aa5502ad18bb991ac173015fe56a42992d709ed1
STB a.k.a. Satomaru <Satomaru@users.noreply.github.com>
2020-10-24 15:07:54 +0900

    ファイル構成を変更。

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const playJsUtils = {
-  api: require('./api-utils')
-};
+module.exports = {
+  toArray: (value) => (Array.isArray(value)) ? value : [value],
 
-module.exports = playJsUtils;
+  isNullish: (value) => value === null || value === undefined,
+
+  compare: (value1, value2) => {
+    const nullishValue1 = apiUtils.isNullish(value1);
+    const nullishValue2 = apiUtils.isNullish(value2);
+
+    if (nullishValue1) {
+      return (nullishValue2) ? 0 : -1;
+    }
+
+    if (nullishValue2) {
+      return 1;
+    }
+
+    if (value1 === value2) {
+      return 0;
+    }
+
+    return (value1 < value2) ? -1: 1;
+  },
+
+  containsOneOf: (array, ...oneOf) => {
+    const expected = oneOf.flat();
+    return array.some((value) => expected.includes(value));
+  }
+}
